ffc6ed0d17d4a6e2dab0b7b5681f38ac635ffe6a
seeunla <107592967+seeunla@users.noreply.github.com>
2023-05-05 15:33:43 +0900

    Feat: BaseEntity 추가

---
+++ src/main/java/com/example/board/webapp/base/entity/BaseEntity.java
@@ -0,0 +1,30 @@
+package com.example.board.webapp.base.entity;
+
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@SuperBuilder
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    private LocalDateTime createDate;
+
+    @LastModifiedDate
+    private LocalDateTime updateDate;
+}
